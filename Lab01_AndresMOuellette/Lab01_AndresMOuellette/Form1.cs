60d4f33f7e9ed9838ada0791ef4bf351054d3ac2
Andres M Ouellette <andres.pmo@hotmail.com>
2017-09-17 19:59:00 -0600

    almost done - home 17/09/17

--- Lab01_AndresMOuellette/Lab01_AndresMOuellette/Form1.cs
+++ Lab01_AndresMOuellette/Lab01_AndresMOuellette/Form1.cs
@@ -13,24 +13,61 @@ namespace Lab01_AndresMOuellette
 {
 	public partial class Form1 : Form
 	{
-        private Thread m_tdecode; //thread control for decoder
-
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
-		private void TSGoButton_Click(object sender, EventArgs e)
+        public delegate void delvoidvoid();                             //for increasing progressbar
+        public delegate void delvoidBitmap(Bitmap bm);                  //for loading new picture
+
+        private void TSGoButton_Click(object sender, EventArgs e)
 		{
+            TSProgressBar.Value = 0;                                    //start the progress bar @ 0
             Bitmap bm = new Bitmap(pictureBox.Image);
-            m_tdecode = new Thread(new ThreadStart(Decode(bm)));
-            m_tdecode.IsBackground = true;
-            m_tdecode.Start();
+            Thread tdecode = new Thread(new ParameterizedThreadStart(Decode));
+            tdecode.IsBackground = true;
+            tdecode.Start(bm);
         }
 
         private void Decode(Bitmap canv)
         {
+            for (int y = 0; y < canv.Height; y++)
+            {
+                for (int x = 0; x < canv.Width; x++)
+                {
+                    byte R = canv.GetPixel(x, y).R;
+                    byte G = canv.GetPixel(x, y).G;
+                    byte B = canv.GetPixel(x, y).B;
 
+                    if (TSRGBComboBox.SelectedItem == "R")
+                    {
+                        if ((R & 1) == 1)
+                        {
+                            canv.SetPixel(x, y, Color.FromArgb(1, 255, 0, 0));
+                        }
+                    }
+                    else if (TSRGBComboBox.SelectedItem == "G")
+                    {
+                        if ((G & 1) == 1)
+                        {
+                            canv.SetPixel(x, y, Color.FromArgb(1, 0, 255, 0));
+                        }
+                    }
+                    else if (TSRGBComboBox.SelectedItem == "B")
+                    {
+                        if ((B & 1) == 1)
+                        {
+                            canv.SetPixel(x, y, Color.FromArgb(1, 0, 0, 255));
+                        }
+                    }
+                    else if (TSRGBComboBox.SelectedItem == "RGB")
+                    {
+
+                    }
+                }
+            }
+            Invoke(new delvoidBitmap(Decode), canv);
         }
 
 		private void TSLoadImgButton_Click(object sender, EventArgs e)
@@ -47,5 +84,5 @@ namespace Lab01_AndresMOuellette
 				MessageBox.Show("Unable to open image!", Text);
 			}
 		}
-	}
+    }
 }

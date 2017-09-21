712ba4dbc0fba5c2f31509e42974646c4a0ca58a
Andres M Ouellette <andres.pmo@hotmail.com>
2017-09-21 11:29:17 -0600

    Add files via upload

---
+++ ICA03_AndresMOuellette/ICA03_AndresMOuellette/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICA03_AndresMOuellette
+{
+    class Ball
+    {
+        static Random Rand = new Random();
+        static CDrawer Canvas = null;
+        public static int Radius
+        {
+            get { return Radius; }
+            set { Math.Abs(value); }
+        }
+        private Color _colour;
+        private Point _point;
+        private int _xSpeed, ySpeed;
+        private int _iAlive;
+
+        static Ball()
+        {
+            Canvas = new CDrawer(Rand.Next(600, 901), Rand.Next())
+        }
+    }
+}

d66df0b01ee70d72eebcae66360c8899cba854b5
Andres M Ouellette <andres.pmo@hotmail.com>
2017-09-21 11:53:13 -0600

    Add files via upload

--- ICA03_AndresMOuellette/ICA03_AndresMOuellette/Ball.cs
+++ ICA03_AndresMOuellette/ICA03_AndresMOuellette/Ball.cs
@@ -10,8 +10,8 @@ namespace ICA03_AndresMOuellette
 {
     class Ball
     {
-        static Random Rand = new Random();
-        static CDrawer Canvas = null;
+        static Random rand = new Random();
+        static CDrawer canvas = null;
         public static int Radius
         {
             get { return Radius; }
@@ -19,12 +19,30 @@ namespace ICA03_AndresMOuellette
         }
         private Color _colour;
         private Point _point;
-        private int _xSpeed, ySpeed;
+        private int _xSpeed, _ySpeed;
         private int _iAlive;
 
         static Ball()
         {
-            Canvas = new CDrawer(Rand.Next(600, 901), Rand.Next())
+            canvas = new CDrawer(rand.Next(600, 901), rand.Next(), false);
+            Radius = rand.Next(10, 81);
+        }
+        public Ball()
+        {
+            _colour = RandColor.GetColor();
+            _xSpeed = rand.Next(-10, 11);
+            _ySpeed = rand.Next(-10, 11);
+            _point.X = (rand.Next(0, canvas.m_ciWidth + 1) - Radius);
+            _point.Y = (rand.Next(0, canvas.m_ciHeight + 1) - Radius);
+        }
+
+        public void ShowBall()
+        {
+            canvas.AddCenteredEllipse(_point, Radius, Radius, Color.FromArgb(_iAlive, _colour));
+        }
+        public void MoveBall()
+        {
+
         }
     }
 }

3cfb55c783ba98f268a6b42bb3d32b859e752ae3
Andres M Ouellette <andres.pmo@hotmail.com>
2017-09-21 20:12:10 -0600

    Add files via upload

--- ICA03_AndresMOuellette/ICA03_AndresMOuellette/Ball.cs
+++ ICA03_AndresMOuellette/ICA03_AndresMOuellette/Ball.cs
@@ -12,19 +12,37 @@ namespace ICA03_AndresMOuellette
     {
         static Random rand = new Random();
         static CDrawer canvas = null;
+        private static int _Radius;
+
         public static int Radius
         {
-            get { return Radius; }
-            set { Math.Abs(value); }
+            get { return _Radius; }
+            set
+            {
+                if (Math.Abs(value) < (canvas.m_ciWidth / 4) && Math.Abs(value) < (canvas.m_ciHeight / 4))
+                    Math.Abs(value);
+            }
         }
+
         private Color _colour;
-        private Point _point;
+        private Point _location;
         private int _xSpeed, _ySpeed;
         private int _iAlive;
 
+        public static bool Loading
+        {
+            set
+            {
+                if (value)
+                    canvas.Clear();
+                else
+                    canvas.Render();
+            }
+        }
+
         static Ball()
         {
-            canvas = new CDrawer(rand.Next(600, 901), rand.Next(), false);
+            canvas = new CDrawer(rand.Next(600, 901), rand.Next(500, 800), false);
             Radius = rand.Next(10, 81);
         }
         public Ball()
@@ -32,17 +50,44 @@ namespace ICA03_AndresMOuellette
             _colour = RandColor.GetColor();
             _xSpeed = rand.Next(-10, 11);
             _ySpeed = rand.Next(-10, 11);
-            _point.X = (rand.Next(0, canvas.m_ciWidth + 1) - Radius);
-            _point.Y = (rand.Next(0, canvas.m_ciHeight + 1) - Radius);
+            _location = new Point((rand.Next(0, canvas.m_ciWidth + 1) - Radius), 
+                (rand.Next(0, canvas.m_ciHeight + 1) - Radius));
         }
 
         public void ShowBall()
         {
-            canvas.AddCenteredEllipse(_point, Radius, Radius, Color.FromArgb(_iAlive, _colour));
+            canvas.AddCenteredEllipse(_location, Radius * 2, Radius * 2, Color.FromArgb(_iAlive, _colour));
         }
         public void MoveBall()
         {
+            _iAlive -= 1;
+            if (_iAlive < 1)
+            {
+                _location = new Point((rand.Next(Radius, canvas.m_ciWidth + 1) - Radius),
+                    (rand.Next(Radius, canvas.m_ciHeight + 1) - Radius));
+                _iAlive = rand.Next(50, 128);
+            }
 
+            if((_location.X + _xSpeed) + Radius > canvas.m_ciWidth || (_location.X + _xSpeed) - Radius < 0)
+            {
+                _xSpeed *= -1;
+            }
+            if ((_location.Y + _ySpeed) + Radius > canvas.m_ciHeight || (_location.Y + _ySpeed) - Radius < 0)
+            {
+                _ySpeed *= -1;
+            }
+            _location.X += _xSpeed;
+            _location.Y += _ySpeed;
+
+            if ((_location.X + Radius) > canvas.m_ciWidth)
+                _location.X -= (_location.X + Radius - canvas.m_ciWidth);   //right
+            if ((_location.X - Radius) < 0)
+                _location.X -= (_location.X - Radius);                      //left
+
+            if ((_location.Y + Radius) > canvas.m_ciHeight)
+                _location.Y -= (_location.Y + Radius - canvas.m_ciHeight);   //down
+            if ((_location.Y - Radius) < canvas.m_ciHeight)
+                _location.X -= (_location.Y - Radius);   //up
         }
     }
 }
--- ICA03_AndresMOuellette/ICA03_AndresMOuellette/Form1.cs
+++ ICA03_AndresMOuellette/ICA03_AndresMOuellette/Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using GDIDrawer;
 
 namespace ICA03_AndresMOuellette
 {
     public partial class Form1 : Form
     {
+        List<Ball> balls = new List<Ball>();
         public Form1()
         {
             InitializeComponent();
+            MouseWheel += Form1_MouseWheel;
+            
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Ball.Radius += (e.Delta / 10);
+        }
+
+        private void ThreadMethod()
+        {
+            while (true)
+            { //***add lock here (ppt has notes on it)
+                lock (balls)
+                {
+                    Ball.Loading = true;
+                    for (int x = 0; x < balls.Count; x++)
+                    {
+                        balls[x].MoveBall();
+                        balls[x].ShowBall();
+                    }
+                    Ball.Loading = false;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            lock (balls)
+            {
+                if (e.KeyCode == Keys.Add)
+                {
+                    for (int x = 5; x > 0; x--)
+                        balls.Add(new Ball());
+                }
+                else if (e.KeyCode == Keys.Clear)
+                    balls.Clear();
+            }
         }
     }
 }

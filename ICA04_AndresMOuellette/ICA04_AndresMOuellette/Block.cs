2cb49430559fc25dafbfbb1f297841cfffb44973
Andres M Ouellette <andres.pmo@hotmail.com>
2017-09-27 15:52:16 -0600

    Add files via upload

---
+++ ICA04_AndresMOuellette/ICA04_AndresMOuellette/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICA04_AndresMOuellette
+{
+    class Block
+    {
+        private static CDrawer _canvas = new CDrawer(800, 600, false);
+        public static CDrawer canvas
+        {
+            get { return (_canvas); }
+        }
+
+        private static Random rand = new Random();
+        private int _blockSize;
+        public int blockSize
+        {
+            set { Math.Abs(value); }
+        }
+        private Color _blockColour = RandColor.GetColor();
+        private Rectangle _blockCoords;
+    }
+}
---
+++ ICA04_AndresMOuellette/ICA04_AndresMOuellette/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ICA04_AndresMOuellette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button.Location = new System.Drawing.Point(12, 14);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(260, 37);
+            this.button.TabIndex = 0;
+            this.button.Text = "Add Blocks: Size - 50";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 57);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(260, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 92);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button);
+            this.MaximumSize = new System.Drawing.Size(300, 131);
+            this.MinimumSize = new System.Drawing.Size(300, 131);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+

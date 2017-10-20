d1423856c76b9b544718704149b4aad9b8787a56
Andres M Ouellette <andres.pmo@hotmail.com>
2017-10-19 20:01:28 -0600

    Add files via upload

---
+++ ICA08_AndresMOuellette/ICA08_AndresMOuellette/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ICA08_AndresMOuellette
+{
+    public partial class Form1 : Form
+    {
+        List<Ball> lGreenBalls = new List<Ball>();
+        List<Ball> lBlueBalls = new List<Ball>();
+        List<Ball> lRedBalls = new List<Ball>();
+
+        public CDrawer outCanvas = null;
+        public CDrawer inCanvas = null;
+        public Form1()
+        {
+            InitializeComponent();
+            inCanvas = new CDrawer(600, 300, true, false);
+            outCanvas = new CDrawer(600, 300, true, false);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Point clickPoint;
+            if (inCanvas.GetLastMouseLeftClickScaled(out clickPoint))
+            {
+                Ball tempBall = new Ball(clickPoint, Color.Green);
+                if (!(lGreenBalls.Contains(tempBall)))
+                {
+                    lGreenBalls.Add(tempBall);
+                }
+            }
+            else if (inCanvas.GetLastMouseRightClickScaled(out clickPoint))
+            {
+                Ball tempBall = new Ball(clickPoint, Color.Blue);
+                if (lBlueBalls.IndexOf(tempBall) <= 0)
+                {
+                    lBlueBalls.Insert(0, tempBall);
+                }
+            }
+
+            foreach(Ball ball in lGreenBalls)
+            {
+                ball.Move(inCanvas);
+                
+            }
+            foreach (Ball ball in lBlueBalls)
+            {
+                ball.Move(inCanvas);
+            }
+            foreach (Ball ball in lRedBalls)
+            {
+                ball.Move(outCanvas);
+            }
+
+            List<Ball> collisions = lGreenBalls.Intersect(lBlueBalls).ToList();
+            foreach(Ball ball in collisions)
+            {
+                lGreenBalls.Remove(ball);
+                lBlueBalls.Remove(ball);
+                ball._colour = Color.Red;
+            }
+
+            lRedBalls = new List<Ball>(lRedBalls.Union(collisions));
+            inCanvas.Clear();
+            outCanvas.Clear();
+            inCanvas.AddText($"Blue: {lBlueBalls.Count()} Green: {lGreenBalls.Count()}", 50, Color.DarkSeaGreen);
+            outCanvas.AddText($"{lRedBalls.Count()}", 50, Color.DarkRed);
+
+            for(int i = 0; i < lGreenBalls.Count(); i++)
+            {
+                lGreenBalls[i].Show(inCanvas, i);
+            }
+            for (int i = 0; i < lBlueBalls.Count(); i++)
+            {
+                lBlueBalls[i].Show(inCanvas, i);
+            }
+            for (int i = 0; i < lRedBalls.Count(); i++)
+            {
+                lRedBalls[i].Show(outCanvas, i);
+            }
+
+            inCanvas.Render();
+            outCanvas.Render();
+        }
+    }
+}
---
+++ ICA08_AndresMOuellette/ICA08_AndresMOuellette/ball.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+
+namespace ICA08_AndresMOuellette
+{
+    class Ball
+    {
+        static Random sRand = new Random();
+        private PointF _centre;
+        private float _xVel;
+        private float _yVel;
+        private int _radius;
+        public Color _colour { get; set; }
+
+        public Ball(PointF inPoint, Color inColour)
+        {
+            _centre = inPoint;
+            _colour = inColour;
+
+            _xVel = (float)sRand.NextDouble() * 10 - 5;
+            _yVel = (float)sRand.NextDouble() * 10 - 5;
+
+            _radius = sRand.Next(20, 51);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ball)) return false;
+            Ball tempBall = obj as Ball;
+
+            float distance = this.GetDistance(tempBall);
+
+            return (distance <= this._radius + tempBall._radius);
+        }
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public float GetDistance(Ball inBall) // |srt((x2 - x1)^2 + (y2 - y1)^2)|
+        {
+            return (float)(Math.Abs(Math.Sqrt(Math.Pow((inBall._centre.X - this._centre.X), 2) +
+                Math.Pow((inBall._centre.Y - this._centre.Y), 2))));
+        }
+
+        public void Move(CDrawer canv)
+        {
+            if ((_centre.X + _xVel + _radius) > canv.m_ciWidth || (_centre.X + _xVel - _radius) < 0) //canv.ActualWidth doesnt work!
+            {
+                //_centre.X = _radius;
+                _xVel *= -1;
+            }
+            if ((_centre.Y + _yVel + _radius) > canv.m_ciHeight || (_centre.Y + _yVel - _radius) < 0) //canv.ActualHeight doesnt work!
+            {
+                //_centre.Y = _radius;
+                _yVel *= -1;
+            }
+
+            _centre.X += _xVel;
+            _centre.Y += _yVel;
+        }
+        public void Show(CDrawer canv, int i)
+        {
+            canv.AddCenteredEllipse((int)_centre.X, (int)_centre.Y, (int)_radius * 2, (int)_radius * 2, _colour);
+            canv.AddText(i.ToString(), 14, (int)_centre.X - _radius, (int)_centre.Y - _radius, _radius * 2, _radius * 2, Color.FromArgb(_colour.ToArgb() ^ 0x00FFFFFF));
+        }
+    }
+}
